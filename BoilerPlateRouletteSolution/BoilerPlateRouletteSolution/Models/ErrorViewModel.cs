4eeb60edc6887c63c1f7cde09eb44acc5b5409e6
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-12-09 04:40:00 -0100

    Started project template, added gitignore.

---
+++ BoilerPlateRouletteSolution/BoilerPlateRouletteSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BoilerPlateRouletteSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
