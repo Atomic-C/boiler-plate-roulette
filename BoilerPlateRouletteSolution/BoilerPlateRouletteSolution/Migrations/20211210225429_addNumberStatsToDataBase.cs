9b3ea28d0bd58addf9485df252427f352b5270d9
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-12-10 22:07:13 -0100

    Added tools nuget package for entity framework core, added migration, fixed typo in appsettings, updated database with migration.

---
+++ BoilerPlateRouletteSolution/BoilerPlateRouletteSolution/Migrations/20211210225429_addNumberStatsToDataBase.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoilerPlateRouletteSolution.Migrations
+{
+    public partial class addNumberStatsToDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NumberStats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EvenOdd = table.Column<bool>(type: "bit", nullable: false),
+                    HotCold = table.Column<bool>(type: "bit", nullable: false),
+                    Percentage = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NumberStats", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NumberStats");
+        }
+    }
+}
