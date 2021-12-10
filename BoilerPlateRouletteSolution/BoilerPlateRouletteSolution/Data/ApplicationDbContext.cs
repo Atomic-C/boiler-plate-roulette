f0ff5e6152dfd1032580d05ad26bf9d38f34f355
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-12-10 20:20:49 -0100

    Added entity framework core sql server and entity framework core was installed by default :o

---
+++ BoilerPlateRouletteSolution/BoilerPlateRouletteSolution/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoilerPlateRouletteSolution.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // DbContext class did not exist for inheritance before we added the nuget package
+
+    }
+}
