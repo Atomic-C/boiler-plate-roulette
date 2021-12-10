2010c22108000dd92572e80c315447554348a029
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-12-10 22:57:23 -0100

    Added View for NumberBets Index, ApplicationDbContext magically appears as we need thanks to DI and objList is printed with a writeline.

--- BoilerPlateRouletteSolution/BoilerPlateRouletteSolution/Controllers/NumberBetsController.cs
+++ BoilerPlateRouletteSolution/BoilerPlateRouletteSolution/Controllers/NumberBetsController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BoilerPlateRouletteSolution.Data;
+using BoilerPlateRouletteSolution.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +11,19 @@ namespace BoilerPlateRouletteSolution.Controllers
 {
     public class NumberBetsController : Controller
     {
+        private readonly ApplicationDbContext _applicationDbContext; // This is dependency injection. Read more about it 1º
+        public NumberBetsController(ApplicationDbContext applicationDbContext) // This is dependency injection. Read more about it 1º
+        {
+            _applicationDbContext = applicationDbContext; // This is dependency injection. Read more about it 1º
+            // This works because we already have added ApplicationDbContext in our services container.
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<NumberStats> objList = _applicationDbContext.NumberStats;
+            Debug.WriteLine("Pedro was here!");
+            Debug.WriteLine(objList);
+
+            return View(objList);
         }
 
         public IActionResult Insertions()
