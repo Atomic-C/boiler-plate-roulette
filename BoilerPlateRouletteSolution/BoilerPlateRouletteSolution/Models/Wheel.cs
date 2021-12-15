e5c5d113735188ade229ef1e6f8ddaac3e9b7553
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-12-14 23:23:06 -0100

    Added Wheel, WheelNumber, WheelNumberColorEnum models to assist with constant values that will never change.

---
+++ BoilerPlateRouletteSolution/BoilerPlateRouletteSolution/Models/Wheel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoilerPlateRouletteSolution.Models
+{
+    public class Wheel
+    {
+        public WheelNumber[] WheelNumbers = { new WheelNumber(1, WheelNumberColorEnum.Black) };
+
+
+    }
+}
---
+++ BoilerPlateRouletteSolution/BoilerPlateRouletteSolution/Models/WheelNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoilerPlateRouletteSolution.Models
+{
+    public class WheelNumber
+    {
+        public int Number { get; }
+        public WheelNumberColorEnum Color { get; }
+
+        public WheelNumber(int number, WheelNumberColorEnum color)
+        {
+                Number = number;
+                Color = color;
+        }
+    }
+}
