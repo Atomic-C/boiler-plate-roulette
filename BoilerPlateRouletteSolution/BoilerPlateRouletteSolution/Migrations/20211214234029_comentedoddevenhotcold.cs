edc463506f9757b2695d568e502d7c206f56bcf4
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-12-14 23:14:34 -0100

    Changed database to hold only Number, Quantity, Color and Percentage values. For now.

---
+++ BoilerPlateRouletteSolution/BoilerPlateRouletteSolution/Migrations/20211214234029_comentedoddevenhotcold.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoilerPlateRouletteSolution.Migrations
+{
+    public partial class comentedoddevenhotcold : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EvenOdd",
+                table: "NumberStats");
+
+            migrationBuilder.DropColumn(
+                name: "HotCold",
+                table: "NumberStats");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EvenOdd",
+                table: "NumberStats",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HotCold",
+                table: "NumberStats",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
